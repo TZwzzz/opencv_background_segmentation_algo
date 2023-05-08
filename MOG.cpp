d466d732ecf7abba1813264dbb53650de0a3018c
TZwzzz <120455615+TZwzzz@users.noreply.github.com>
2023-05-08 17:24:53 +0800

    Create MOG.cpp

---
+++ MOG.cpp
@@ -0,0 +1,30 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/bgsegm.hpp>
+using namespace cv;
+
+int main()
+{
+    VideoCapture cap("../cup.mp4");
+    Ptr<bgsegm::BackgroundSubtractorMOG> fgbg = bgsegm::createBackgroundSubtractorMOG();
+    
+    while (true)
+    {
+        Mat frame, fgmask;
+        bool ret = cap.read(frame);
+        if (!ret) break;
+
+        // 应用背景建模算法
+        fgbg->apply(frame, fgmask);
+
+        // 显示结果
+        imshow("frame", fgmask);
+
+        // 等待键盘输入
+        int k = waitKey(30) & 0xff;
+        if (k == 27) break;
+    }
+
+    cap.release();
+    destroyAllWindows();
+    return 0;
+}
